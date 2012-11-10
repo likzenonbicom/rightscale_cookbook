b370d4e1c6639848940dd0c90ef15804ebebb920
Sergey <lik@lik-ubuntu.(none)>
2012-11-10 09:46:05 +0700

    adde package

--- openvpn/metadata.rb
+++ openvpn/metadata.rb
@@ -1,6 +1,10 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "ZenonBI.com"
+maintainer_email "lik@zenonbi.com"
 license          "All rights reserved"
 description      "Installs/Configures openvpn"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
+
+supports "ubuntu", "~> 10.04"
+
+depends "app"
--- openvpn/recipes/default.rb
+++ openvpn/recipes/default.rb
@@ -2,7 +2,16 @@
 # Cookbook Name:: openvpn
 # Recipe:: default
 #
-# Copyright 2012, YOUR_COMPANY_NAME
+# Copyright 2012, ZenonBI
 #
 # All rights reserved - Do Not Redistribute
 #
+
+rightscale_marker :begin
+
+log " Setting provider specific settings for openvpn"
+node[:app][:provider] = "openvpn"
+
+node[:app][:packages] = {"default" => ["openvpn"]}
+
+rightscale_marker :end
